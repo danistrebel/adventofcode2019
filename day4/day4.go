ac7a1c31d68543e36f054c0db82850082262cd2f
Daniel Strebel <daniel.strebel@gmail.com>
2021-11-07 18:44:57 +0100

    Day 4 Part 2

--- day4/day4.go
+++ day4/day4.go
@@ -2,7 +2,7 @@ package day4
 
 import "fmt"
 
-func fitsPasswordRequirements(input int) bool {
+func fitsPasswordRequirementsPart1(input int) bool {
 	prev := input
 	sawDouble := false
 	for i := 0; i < 6; i++ {
@@ -19,12 +19,43 @@ func fitsPasswordRequirements(input int) bool {
 	return sawDouble
 }
 
-func PrintSolution() {
-	passowordCount := 0
-	for i := 123257; i <= 647015; i++ {
-		if fitsPasswordRequirements(i) {
-			passowordCount++
+func fitsPasswordRequirementsPart2(input int) bool {
+	prev := input
+
+	counts := make(map[int]int)
+
+	for i := 0; i < 6; i++ {
+		current := input % 10
+		if current > prev {
+			return false
+		}
+		counts[current]++
+		prev = current
+		input = input / 10
+	}
+
+	for _, v := range counts {
+		if v == 2 {
+			return true
 		}
 	}
-	fmt.Println("There are ", passowordCount, " different passwords in that range (Part 1)")
+
+	return false
+}
+
+func PrintSolution() {
+	passowordCount1 := 0
+	passowordCount2 := 0
+
+	for i := 123257; i <= 647015; i++ {
+		if fitsPasswordRequirementsPart1(i) {
+			passowordCount1++
+		}
+		if fitsPasswordRequirementsPart2(i) {
+			passowordCount2++
+		}
+	}
+	fmt.Println("There are ", passowordCount1, " different passwords in that range (Part 1)")
+	fmt.Println("There are ", passowordCount2, " different passwords in that range (Part 2)")
+
 }
--- day4/day4_test.go
+++ day4/day4_test.go
@@ -2,14 +2,26 @@ package day4
 
 import "testing"
 
-func TestPasswordRequirements(t *testing.T) {
-	testPassword(111111, true, t)
-	testPassword(223450, false, t)
-	testPassword(123789, false, t)
+func TestPasswordRequirementsPart1(t *testing.T) {
+	testPasswordPart1(111111, true, t)
+	testPasswordPart1(223450, false, t)
+	testPasswordPart1(123789, false, t)
 }
 
-func testPassword(pass int, shouldPass bool, t *testing.T) {
-	if fitsPasswordRequirements(pass) != shouldPass {
+func testPasswordPart1(pass int, shouldPass bool, t *testing.T) {
+	if fitsPasswordRequirementsPart1(pass) != shouldPass {
+		t.Errorf("Password %d should have been checked with result %t", pass, shouldPass)
+	}
+}
+
+func TestPasswordRequirementsPart2(t *testing.T) {
+	testPasswordPart2(112233, true, t)
+	testPasswordPart2(123444, false, t)
+	testPasswordPart2(111122, true, t)
+}
+
+func testPasswordPart2(pass int, shouldPass bool, t *testing.T) {
+	if fitsPasswordRequirementsPart2(pass) != shouldPass {
 		t.Errorf("Password %d should have been checked with result %t", pass, shouldPass)
 	}
 }
