09d7a75567e8ccbe6e6e70a9b5d65acc3b96085c
Daniel Strebel <daniel.strebel@gmail.com>
2021-11-20 14:50:12 +0100

    Day 6 Part 2

--- day6/day6.go
+++ day6/day6.go
@@ -10,17 +10,60 @@ func buildOrbitGraph(links []string) map[string][]string {
 	orbitGraph := make(map[string][]string)
 	for _, l := range links {
 		rel := strings.Split(l, ")")
-		if prev, ok := orbitGraph[rel[0]]; ok {
-			orbitGraph[rel[0]] = append(prev, rel[1])
+		k := rel[0]
+		v := rel[1]
+		if prev, ok := orbitGraph[k]; ok {
+			orbitGraph[k] = append(prev, v)
 		} else {
-			orbitGraph[rel[0]] = []string{rel[1]}
+			orbitGraph[k] = []string{v}
 		}
+	}
+	return orbitGraph
+}
+
+func buildReverseOrbitGraph(links []string) map[string]string {
+	orbitGraph := make(map[string]string)
+	for _, l := range links {
+		rel := strings.Split(l, ")")
+		orbitGraph[rel[1]] = rel[0]
 	}
 	return orbitGraph
 }
 
 func totalOrbitSize(orbitGraph map[string][]string) int {
 	return tallySizes(orbitGraph, "COM", 0)
+}
+
+func transfersCount(reverseGraph map[string]string) int {
+	yPath := rootPath(reverseGraph, "YOU")
+	sPath := rootPath(reverseGraph, "SAN")
+
+	prefixLength := 0
+	for {
+		if prefixLength < len(yPath) && prefixLength < len(sPath) && yPath[prefixLength] == sPath[prefixLength] {
+			prefixLength++
+		} else {
+			break
+		}
+	}
+
+	return len(sPath) - prefixLength + len(yPath) - prefixLength - 2
+}
+
+func rootPath(reverseGraph map[string]string, leaf string) []string {
+	path := []string{}
+
+	curr := leaf
+	for {
+		path = append([]string{curr}, path...)
+		parent, ok := reverseGraph[curr]
+		if !ok {
+			break
+		} else {
+			curr = parent
+		}
+	}
+	return path
 }
 
 func tallySizes(orbitGraph map[string][]string, orbitCenter string, hopsFromCenter int) int {
@@ -36,4 +79,8 @@ func PrintSolution() {
 	graph := buildOrbitGraph(lines)
 	totalSize := totalOrbitSize(graph)
 	fmt.Println("Total Orbit Size (part 1)", totalSize)
+	reverseGraph := buildReverseOrbitGraph(lines)
+	transferCounts := transfersCount(reverseGraph)
+	fmt.Println("TransferCounts (part 2)", transferCounts)
+
 }
--- day6/day6_test.go
+++ day6/day6_test.go
@@ -12,3 +12,25 @@ func TestToalOrbit(t *testing.T) {
 		t.Error("Got", calculated, "expected 42")
 	}
 }
+
+func TestTransfersCount(t *testing.T) {
+	graph := buildReverseOrbitGraph([]string{
+		"COM)B",
+		"B)C",
+		"C)D",
+		"D)E",
+		"E)F",
+		"B)G",
+		"G)H",
+		"D)I",
+		"E)J",
+		"J)K",
+		"K)L",
+		"K)YOU",
+		"I)SAN"})
+
+	calculated := transfersCount(graph)
+	if calculated != 4 {
+		t.Error("Got", calculated, "expected 4")
+	}
+}
